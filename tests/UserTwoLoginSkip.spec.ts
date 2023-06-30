0fcefacf94052e692a76ae20c17e46ae318017fb
Suneel Prajapati <113509116+Sunil4141@users.noreply.github.com>
2023-06-30 13:55:50 +0530

    commit for update

--- tests/UserTwoLoginSkip.spec.ts
+++ tests/UserTwoLoginSkip.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test("Code for Website Login Skip", async ({ browser }) => {
   const context = await browser.newContext({
-    storageState: "./auth.json",
+    storageState: "./twoauth.json",
   });
   const page = await context.newPage();
   await page.goto("https://testmovescoutpro.sirva.com/account/login");
