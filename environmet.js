e9cbfe2ea4455737c346c957d51edb44ab2ff7ae
Suneel Prajapati <113509116+Sunil4141@users.noreply.github.com>
2023-07-04 17:58:26 +0530

    commit for fresh

--- environmet.js
+++ environmet.js
@@ -1,8 +1,12 @@
 // environment.js
 const Credential = {
-    baseUrl: "www.step.com",
+    baseUrl: "https://testmovescoutpro.sirva.com/account/login",
     Admin_Id: "admin",
-    Admin_Password: "123qwe"
+    Admin_Password: "123qwe",
+    User_Id_One:"Q2222222519",
+    User_Id_Two:"Q2222000236",
+    User_Id_One_Password:"123qwe",
+    User_Id_Two_Password:"123qwe"
   };
   
   module.exports = {
