7441973f1f99355f599f7bc3f004a96c236c9a06
Suneel Prajapati <113509116+Sunil4141@users.noreply.github.com>
2023-07-04 14:15:56 +0530

    commit for check

--- tests/AdminLoginSkip.spec.ts
+++ tests/AdminLoginSkip.spec.ts
@@ -8,7 +8,10 @@ test("Admin >> Test for Website Login Skip", async ({ browser }) => {
     // Validate that the login was successful
     const page = browser.contexts()[0].pages()[0];
     await page.waitForTimeout(2000);
-    
+    await page.getByRole('link', { name: 'Calendar' }).click();
+    await page.waitForTimeout(2000);
+    await page.getByRole('link', { name: 'Lead Management' }).click();
+  
 
 });
 
