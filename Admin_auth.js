3d5084c56ddb87182d2bbfacffa01671ac72f057
Suneel Prajapati <113509116+Sunil4141@users.noreply.github.com>
2023-07-06 12:35:54 +0530

    commit for update

--- Admin_auth.js
+++ Admin_auth.js
@@ -1,6 +1,6 @@
 const { chromium } = require('playwright');
 const fs = require('fs');
-const environment = require('../PlaywrightAutomation/utils/environmet.json');
+const environment = require('./utils/environmet.json');
 
 
 async function generateAuthJSON() {
