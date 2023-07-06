4057b8f0195d29e5e7551b5a2a4270e46df3abf5
Suneel Prajapati <113509116+Sunil4141@users.noreply.github.com>
2023-07-06 10:21:37 +0530

    commit for change

--- tests/AdminLoginSkip.spec.ts
+++ tests/AdminLoginSkip.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 import { loginTestCodeforAdmin } from '../common.spec';
 
 test("Admin >> Test for Website Login Skip", async ({ browser }) => {
+    test.setTimeout(20000)
     await loginTestCodeforAdmin({ browser });
     // Validate that the login was successful
     const page = browser.contexts()[0].pages()[0];
